c9cab2b508edbc9a3468862624364b64493ab846
kaztro <00229017@uca.edu.sv>
2018-10-27 00:49:12 -0600

    The 'Folder' HTML is now finished and some other changes in layout (I'm using Template Inheritance)

--- myapp/routes/folder.js
+++ myapp/routes/folder.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET File */
+/* GET Folder */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    res.render('folder', { title: 'Folder' });
 });
-
+//El nombre del folder cambiara segun el nombre que escoja el usuario... Aun no
 module.exports = router;
