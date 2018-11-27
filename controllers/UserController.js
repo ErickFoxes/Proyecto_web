f7070a25b9005461b70c4fce688003574e0d5b7c
kaztro <00229017@uca.edu.sv>
2018-11-27 00:20:37 -0600

    Solo se ordeno UserController... Nada relevante

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -137,15 +137,7 @@ UserController.logout = function (req, res, next) {
         });
     }
 };
-/*
-UserController.Uploads = function (req, res) {
-    let EDFile = req.files.file
-    EDFile.mv(`./img/${EDFile.name}`,err => {
-        if(err) return res.status(500).send({ message : err })
-        return res.status(200).send({ message : 'File upload' })
-    })
-};
-*/
+
 UserController.multerF = function (req, res, next) {
     return storeWithOriginalName(req.file)
         .then(encodeURIComponent)
@@ -154,6 +146,11 @@ UserController.multerF = function (req, res, next) {
         })
         .catch(next)
 }
+
+UserController.success = function (req, res, next) {
+    var { fileName } = req.query
+    res.render('uploadOk', { fileName })
+};
 
 function storeWithOriginalName(file) {
     var fullNewPath = path.join(file.destination, file.originalname)
@@ -165,71 +162,4 @@ function storeWithOriginalName(file) {
         })
 }
 
-
-UserController.success = function (req, res, next) {
-    var { fileName } = req.query
-    res.render('uploadOk', { fileName })
-};
-
 module.exports = UserController;
-/**** Version antigua del controlador para actualizar.. ***********/
-/*
-UserController.update = function(req,res){
-    if (req.params.id) {
-        User.findByIdAndUpdate(req.params.id,{
-            username: req.body.username,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            password: req.body.password,
-            email: req.body.email,
-            Gender: req.body.Gender,
-            birthday: req.body.birthday,
-            country: req.body.country,
-        },function(err,updated){
-            if (err){
-                res.json({
-                    status: 500,
-                    success: false,
-                    errs
-                });
-            } else{
-                res.json({
-                    status: 200,
-                    success: true,
-                    updated
-                })
-            }
-        });
-    }
-};
-*/
-/*************Version antigua del controlador delete ********************/
-/* 
-UserController.delete = function (req, res) {
-    if (req.params.id) {
-        User.findByIdAndRemove(req.params.id, function (err, user) {
-            if (err) {
-                res.status(500);
-                res.json({
-                    status: 500,
-                    success: false,
-                    err
-                });
-            } else {
-                res.json({
-                    success: true,
-                    delete: user
-                });
-            }
-        });
-    } else {
-
-        res.status(400);
-        res.json({
-            status: 400,
-            success: false
-        });
-    }
-};
-
-*/
